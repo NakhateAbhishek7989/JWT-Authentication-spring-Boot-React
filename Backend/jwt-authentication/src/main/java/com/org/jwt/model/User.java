edbbedee06f115f5d8b98037f771de94e401006c
NakhateAbhishek7989 <nakhateabhishek08759@gmail.com>
2026-03-02 15:22:07 +0530

    Fixed nested repo and removed metadata

---
+++ Backend/jwt-authentication/src/main/java/com/org/jwt/model/User.java
@@ -0,0 +1,32 @@
+package com.org.jwt.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	@Column(unique = true, nullable = false)
+	private String email;
+	
+	private String password;
+	
+	private String role;
+	
+	
+}
